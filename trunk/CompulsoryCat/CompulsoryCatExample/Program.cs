3964c4c6591dd16ce60673c3075053f2bcc54677
ola.fjelddahl@gmail.com <ola.fjelddahl@gmail.com@18fb0858-ca4b-b57d-7aa5-866f682bf9ba>
2010-10-08 14:44:12 +0000

    First commit!

---
+++ trunk/CompulsoryCat/CompulsoryCatExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CompulsoryCat;
+
+namespace CompulsoryCatExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("In a method we can ask for its name with a method call. [{0}]",
+                                            ReflectionUtility.GetMethod().Name));
+            Console.WriteLine(string.Format("or its class. [{0}]", ReflectionUtility.GetClass().Name));
+            Console.WriteLine(string.Format("note that we get the whole Info so we can gete more [{0}]", ReflectionUtility.GetClass()));
+            Console.WriteLine();
+
+            var o = new MyClass();
+            o.MyMethod();
+            MyStaticClass.MyStaticMethod();
+
+            Console.WriteLine();
+            Console.WriteLine( "The ReflectionUtility class contains more good stuff.");
+            Console.WriteLine( "But there are also big holes in it.  There are problems with getting data from a static method or class" +
+                " outside it.");
+
+            Console.WriteLine();
+            Console.Write("Press any key");
+            Console.ReadKey();
+        }
+    }
+}
