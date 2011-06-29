e418a1d16108b7a079a68c68a6bf3f5ae2ca6846
ola.fjelddahl <ola.fjelddahl@18fb0858-ca4b-b57d-7aa5-866f682bf9ba>
2011-06-29 23:13:44 +0000

    Version 1.2 Added this document. Added the Assemblyname functionality. There are no unit tests for this new functionality but they are left as inconclusive. Set the version to 1.2.* to get automatic increase. Added help files to SVN.

---
+++ CompulsoryCat/CompulsoryCat/Assemblyname/AssemblynameTreeAndList.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CompulsoryCat.Assemblyname
+{
+    ///<summary>This class contains two properties.  One is a tree of AssemblyNames and one is a list of them.
+    ///</summary>
+    public class AssemblynameTreeAndList
+    {
+        private readonly AssemblynameHelper.AssemblynameNode _info;
+
+        /// <summary>We have a Dictionary instead of the simpler List since we want to be sure the list is unique
+        /// without having to do or write explicit tests.
+        /// </summary>
+        private readonly Dictionary<string, AssemblyName> _assemblynameList;
+
+        /// <summary>This list of AssemblyNames has unique Assemblies.
+        /// </summary>
+        public List<AssemblyName> AssemblynameList
+        {
+            get { return _assemblynameList.Values.ToList(); }
+        }
+
+        /// <summary>This tree of AssemblyNames has each AssemblyName with its referenced Assemblies.
+        /// </summary>
+        public AssemblynameHelper.AssemblynameNode Tree
+        {
+            get { return _info; }
+        }
+
+        /// <summary>This constructor takes all data and is the prefered one.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="assemblynameList"></param>
+        internal AssemblynameTreeAndList(AssemblynameHelper.AssemblynameNode info, List<AssemblyName> assemblynameList)
+        {
+            _info = info;
+            _assemblynameList = new Dictionary<string, AssemblyName>();
+            assemblynameList.ForEach(an => _assemblynameList.Add(an));
+        }
+    }
+
+    /// <summary>This is a helper class for the Dictionary we use.
+    /// </summary>
+    internal static class DictionaryExtensions
+    {
+        /// <summary>This helper extension adds the Add method that should have been there from start.
+        /// </summary>
+        /// <param name="me"></param>
+        /// <param name="assemblyname"></param>
+        internal static void Add(this Dictionary<string, AssemblyName> me, AssemblyName assemblyname)
+        {
+            me.Add(assemblyname.FullName, assemblyname);
+        }
+    }
+}
